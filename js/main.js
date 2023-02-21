a5194c1b9d8bda5c47d99ef35f4d1bfd3193b158
AbaevB <abaev70@hotmail.com>
2023-02-21 23:07:24 +0700

    #1 Markup start

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
  document.addEventListener('DOMContentLoaded', function(){
-    
- });
+    var popover = new bootstrap.Popover(document.querySelector('.example-popover'), {
+        trigger: 'focus'
+      })
+});
