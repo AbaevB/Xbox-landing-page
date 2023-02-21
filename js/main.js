6764e2bedbe33527d5c0c737f05fb158d38f6e73
AbaevB <abaev70@hotmail.com>
2023-02-22 02:03:18 +0700

    feat(markup): 1# complete

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
  document.addEventListener('DOMContentLoaded', function(){
-    var popover = new bootstrap.Popover(document.querySelector('.example-popover'), {
-        trigger: 'focus'
-      })
+    
 });
